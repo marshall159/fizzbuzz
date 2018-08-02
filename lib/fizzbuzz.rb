841d5efd8dacd0cbd57d7e235c3ca008fe48c117
Aneel Marshall <aneel.marshall@outlook.com>
2018-08-02 17:07:26 +0100

    Add test for 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,9 @@
 def fizzbuzz(number)
-	"fizz"
+	if number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  else
+    number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,10 @@ describe "fizzbuzz" do
 	it 'returns "fizz" when passed 3' do
 		expect(fizzbuzz(3)).to eq "fizz"
 	end
+	it 'returns 4 when passed 4' do
+		expect(fizzbuzz(4)).to eq 4
+	end
+	it 'returns "buzz" when passed 5' do
+		expect(fizzbuzz(5)).to eq "buzz"
+	end
 end
